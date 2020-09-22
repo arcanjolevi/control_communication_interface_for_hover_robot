2ae676f23c7691ab5d6e21e9336a9e3c38a5ce53
Levi <arcanjolevi@gmail.com>
2020-09-22 11:00:56 -0400

    remove mobile folder

--- mobile_app/src/screens/router/RouterTypesDefinition.tsx
+++
@@ -1,9 +0,0 @@
-type RootDrawerParamList = {
-  Control: undefined;
-  ComunicationSettings: undefined;
-  AutoModeSettings: undefined;
-  Info: undefined;
-  Instructions: undefined;
-};
-
-export default RootDrawerParamList;
